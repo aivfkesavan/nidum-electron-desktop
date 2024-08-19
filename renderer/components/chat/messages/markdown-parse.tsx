dbfcf597e3b2350906f28f70ee43affc3767da67
rajkumar <rajkumar@aivf.io>
2024-08-19 18:17:27 +0530

    jor bug fix

--- renderer/components/chat/messages/markdown-parse.tsx
+++ renderer/components/chat/messages/markdown-parse.tsx
@@ -1,16 +1,15 @@
-// import MarkdownPreview from '@uiw/react-markdown-preview';
+import MarkdownPreview from '@uiw/react-markdown-preview';
 
 function MarkdownParse({ response = "" }) {
   return (
-    <div>{response}</div>
-    // <MarkdownPreview
-    //   source={response}
-    //   style={{ background: "transparent", fontSize: ".8rem" }}
-    //   wrapperElement={{
-    //     "data-color-mode": "dark"
-    //   }}
-    //   className='[&_ul]:list-decimal [&_ol]:list-decimal'
-    // />
+    <MarkdownPreview
+      source={response}
+      style={{ background: "transparent", fontSize: ".8rem" }}
+      wrapperElement={{
+        "data-color-mode": "dark"
+      }}
+      className='[&_ul]:list-decimal [&_ol]:list-decimal'
+    />
   )
 }
 
--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type { AppProps } from "next/app";
 
+import "../styles/loader.css";
+import "../styles/animate.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
