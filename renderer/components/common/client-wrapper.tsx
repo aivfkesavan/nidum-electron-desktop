b3793c440f00535ce71aa2f48f4203776e996763
rajkumar <rajkumar@aivf.io>
2024-08-22 16:55:09 +0530

    download provider draft

--- renderer/components/common/client-wrapper.tsx
+++ renderer/components/common/client-wrapper.tsx
@@ -1,6 +1,8 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 
 import { queryClient } from "@/lib/query-client";
+
+import { Toaster as SonnerToaster } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 
 type props = Readonly<{
@@ -13,6 +15,7 @@ function ClientWrapper({ children }: props) {
       <QueryClientProvider client={queryClient}>
         {children}
         <Toaster />
+        <SonnerToaster />
       </QueryClientProvider>
     </>
   )
