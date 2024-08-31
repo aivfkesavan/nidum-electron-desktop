71d6d81eb565128e46562345aff672438e5421f4
rajkumar <rajkumar@aivf.io>
2024-08-31 18:49:51 +0530

    image proccessing main flow

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -1,18 +1,31 @@
 import express from 'express';
 import { promises as fs } from 'fs';
+import imageToBase64 from 'image-to-base64';
 
 import { createPath } from "../utils/path-helper";
 import { upload } from "../middleawres/upload";
 
 const router = express.Router()
 
-router.get('/:folderName/:imageName', (req, res) => {
+router.get('/:folderName/:imageName', async (req, res) => {
   const { folderName, imageName } = req.params
   const imagePath = createPath([folderName, imageName])
 
   res.sendFile(imagePath, (err) => {
-    if (err) return res.status(404).send('Image not found')
+    if (err) return res.status(404).json({ msg: 'Image not found' })
   })
+})
+
+router.get('/to-base64/:folderName/:imageName', async (req, res) => {
+  const { folderName, imageName } = req.params
+  const imagePath = createPath([folderName, imageName])
+
+  try {
+    const converted = await imageToBase64(imagePath)
+    return res.json({ converted })
+  } catch (error) {
+    return res.status(404).json({ msg: "cannot convert into base64" })
+  }
 })
 
 router.post("/:folderName", upload.array('images'), async (req, res) => {
