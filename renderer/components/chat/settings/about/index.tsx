4392effa2949e48a86795bc11b31c9cda831be80
rajkumar <rajkumar@aivf.io>
2024-09-03 21:26:57 +0530

    about content change

--- renderer/components/chat/settings/about/index.tsx
+++ renderer/components/chat/settings/about/index.tsx
@@ -1,13 +1,65 @@
+import { FaXTwitter, FaLinkedin } from "react-icons/fa6";
+import { FaTelegramPlane } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+
+const list = [
+  {
+    id: "1",
+    icon: <FaXTwitter className="text-3xl" />,
+    link: "https://x.com/AiNidum",
+  },
+  {
+    id: "2",
+    icon: <MdOutlineEmail className="text-4xl" />,
+    link: "info@nidum.ai",
+  },
+  {
+    id: "3",
+    icon: <FaTelegramPlane className="text-4xl" />,
+    link: "https://t.me/nidumai",
+  },
+  {
+    id: "4",
+    icon: <FaLinkedin className="text-3xl" />,
+    link: "https://www.linkedin.com/company/nidum-ai",
+  },
+]
 
 function About() {
   return (
     <div className="text-xs text-white/80">
-      <h2 className="mb-4 text-base text-white/90">Welcome</h2>
-      <p className="mb-4">Unlock the future of AI with Nidum, the platform that empowers you to build and deploy personalized AI agents with unmatched ease.</p>
-      <p className="mb-4">We blend cutting-edge technology with decentralized cloud infrastructure to give you on-demand compute power for AI applications of any scale. Whether you're enhancing customer support, automating complex workflows, or diving into new industries, Nidum AI puts control back into your hands—without needing to code.</p>
-      <p>Experience the freedom of scalable, secure, and localized AI solutions that evolve with your business.</p>
+      {/* <h2 className="mb-4 text-base text-white/90">Welcome</h2> */}
+      <p className="mb-4">Current Version: RAGDrive Version 1.0.5.</p>
+      <p className="mb-4">Hi there! We are a small bootstrapped startup of mostly sober geeks and we hope you have as much fun using RAGDrive as we did creating this for you.</p>
+      <p className="mb-4">Our goal is to bring AI with RAG and Agents to all. Not just the developers and AI enthusiasts but every end user. We are right now the World’s First and ONLY edge no-code Local AI Studio that supports Documents and Image RAG.</p>
+      <p className="mb-4">What's in the next update:</p>
+      <p className="mb-4">Our GenAI Store is going to be launched soon in this same app you're using. It will allow thousands of developers to build and launch apps that can be used inside of this no-code AI Studio. A promising startup have already been greenlit by our community to launch their AI  Chat Bot that you can use on your website to support your customers/leads via Zendesk, Salesforce, Zoho, Freshworks, Shopify, LiveChatInc and more. With this, you can setup autonomous agents that run on your knowledge base on your computer which will be free forever, helping you supercharge your business.</p>
+      <p className="mb-4">There will also be Text to Image and Video support in your RAGDrive in the near future.</p>
+      <p className="mb-1">If you need support, please use any one of the following channels:</p>
+
+      <p className="df gap-4 mb-5">
+        {
+          list.map(l => (
+            <a
+              key={l.id}
+              href={l.link}
+              className="hover:text-white"
+              target="_blank"
+            >
+              {l.icon}
+            </a>
+          ))
+        }
+      </p>
+
+      <p className="mb-4">If you want to launch your plugins in RAGDrive, get in touch with us.</p>
     </div>
   )
 }
 
 export default About
+
+// Twitter/X: https://x.com/AiNidum
+// Email: info@nidum.ai
+// Telegram: https://t.me/nidumai 
+// LinkedIn: https://www.linkedin.com/company/nidum-ai 
