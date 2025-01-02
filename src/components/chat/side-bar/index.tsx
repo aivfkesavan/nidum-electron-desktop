a0ad76791f0b3e1a386c1f90729a85611b2bfa51
rajkumar <rajkumar@aivf.io>
2025-01-02 11:28:19 +0530

    sidebar collapsible button

--- src/components/chat/side-bar/index.tsx
+++ src/components/chat/side-bar/index.tsx
@@ -1,3 +1,6 @@
+import { CSSProperties } from "react";
+import { RiArrowLeftWideFill } from "react-icons/ri";
+
 import useIsFullScreenCheck from "../../../hooks/use-is-full-screen-check";
 import usePlatform from "../../../hooks/use-platform";
 import useResizable from "./use-resizable";
@@ -19,22 +22,32 @@ function SideBar() {
   return (
     <>
       <div
-        className="side-nav flex w-[240px] shrink-0 overflow-hidden border-r bg-[#171717] z-[1] transition-transform duration-200 border-t"
+        className="side-nav w-[240px] shrink-0 border-r bg-[#171717] z-[1] transition-transform duration-200 border-t"
         style={{ width: resizedWidth }}
       >
-        <Projects
-          isFullScreen={isFullScreen}
-          platform={platform}
-        />
-        <Histories
-          isFullScreen={isFullScreen}
-          platform={platform}
-        />
+        <div className="flex overflow-hidden">
+          <Projects
+            isFullScreen={isFullScreen}
+            platform={platform}
+          />
+          <Histories
+            isFullScreen={isFullScreen}
+            platform={platform}
+          />
 
-        <div
-          className="w-px h-screen absolute right-0 bg-secondary cursor-ew-resize"
-          {...resizeHandle}
-        ></div>
+          <div
+            className="w-px h-screen absolute right-0 bg-secondary cursor-ew-resize"
+            {...resizeHandle}
+          ></div>
+        </div>
+
+        <button
+          className="px-0 py-5 opacity-0 [.open_&]:opacity-100 select-none [.open_&]:select-auto text-xl absolute top-1/2 left-full -translate-y-1/2 bg-[#444] z-[1] rounded-l-none rounded-r-xl transition-opacity"
+          style={{ "--x": `${resizedWidth}px` } as CSSProperties}
+          onClick={() => document.body.classList.toggle("open")}
+        >
+          <RiArrowLeftWideFill />
+        </button>
       </div>
 
       <SideNavToggler />
